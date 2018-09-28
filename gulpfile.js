c982fd7cbbeb82d6c7040f1b9fc960c5b124c761
sl204984 <17798520003@163.com>
2018-09-28 17:30:12 +0800

    git

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,13 @@ gulp.task('default', function () {
   return gulp.src('images/*.png') //需要合并的图片地址
     .pipe(spritesmith({
       imgName: 'sprite.png', //保存合并后图片的地址
-      cssName: 'css/sprite.json', //保存合并后对于css样式的地址
+      cssName: 'sprite.json', //保存合并后对于css样式的地址
       padding: 5, //合并时两个图片的间距
       algorithm: 'binary-tree', //注释1
       cssTemplate: function (data) {
         var arr = [];
         data.sprites.forEach(function (sprite) {
-          arr.push(`"${sprite.name}":{"width":${sprite.width},"height":${sprite.height},"x":${sprite.offset_x},"y":${sprite.offset_y}, "pixelRatio": 2,"visible": true}`);
+          arr.push(`"${sprite.name}":{"width":${sprite.width},"height":${sprite.height},"x":${-sprite.offset_x},"y":${-sprite.offset_y}, "pixelRatio": 2,"visible": true}`);
           //   arr.push(sprite.name +
           //     "{" +
           //     "background-image: url('" + sprite.escaped_image + "');" +
