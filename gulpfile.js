3c1d79176cfb5605b7d847b6f6c53db5b9f82d33
sl204984 <17798520003@163.com>
2018-11-07 09:25:40 +0800

    提交

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,20 @@
 //引入gulp
-var gulp = require("gulp"),
-  spritesmith = require('gulp.spritesmith');
+const fs = require('fs');
+const gulp = require("gulp");
+const spritesmith = require('gulp.spritesmith');
 
 gulp.task('default', function () {
 
-  return gulp.src('images/*.png') //需要合并的图片地址
+  return gulp.src('images-kind/*.png') //需要合并的图片地址
     .pipe(spritesmith({
       imgName: 'sprite.png', //保存合并后图片的地址
       cssName: 'sprite.json', //保存合并后对于css样式的地址
       padding: 5, //合并时两个图片的间距
       algorithm: 'binary-tree', //注释1
       cssTemplate: function (data) {
-        var arr = [];
+        const arr = [];
         data.sprites.forEach(function (sprite) {
           arr.push(`"${sprite.name}":{"width":${sprite.width},"height":${sprite.height},"x":${-sprite.offset_x},"y":${-sprite.offset_y}, "pixelRatio": 2,"visible": true}`);
-          //   arr.push(sprite.name +
-          //     "{" +
-          //     "background-image: url('" + sprite.escaped_image + "');" +
-          //     "background-position: " + sprite.px.offset_x + "px " + sprite.px.offset_y + "px;" +
-          //     "width:" + sprite.px.width + ";" +
-          //     "height:" + sprite.px.height + ";" +
-          //     "}\n");
         });
         return "{" + arr.join(",") + "}";
       }
@@ -28,3 +22,7 @@ gulp.task('default', function () {
     }))
     .pipe(gulp.dest('dist/'));
 });
+
+// 复制
+fs.writeFileSync('./dist/sprite@2x.json', fs.readFileSync('./dist/sprite.json'));
+fs.writeFileSync('./dist/sprite@2x.png', fs.readFileSync('./dist/sprite.png'));
